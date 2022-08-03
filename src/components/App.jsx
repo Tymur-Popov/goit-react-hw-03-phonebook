717a4d2acb1ed672e38970933f70127b9317fe85
Tymur Popov <bs2timur@gmail.com>
2022-08-03 20:22:38 +0300

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+import { nanoid } from 'nanoid';
+import './App.module.css';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  formSubmit = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.state.contacts.some(
+      i =>
+        (i.name.toLowerCase() === contact.name.toLowerCase() &&
+          i.number === contact.number) ||
+        i.number === contact.number
+    )
+      ? alert(`${name} is already in contacts`)
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
+  };
+
+  changeFilterInput = e => {
+    this.setState({ filter: e.target.value });
+  };
+
+  findContacts = () => {
+    const { filter, contacts } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    return (
+      <section>
+        <div>
+          <h1>Phonebook</h1>
+          <ContactForm onSubmit={this.formSubmit} />
+          <h2>Contacts</h2>
+          <Filter filter={this.state.filter} changeFilterInput={this.changeFilterInput}/>
+          <ContactList
+            contacts={this.findContacts()}
+            deleteContact={this.deleteContact}
+          />
+        </div>
+      </section>
+    );
+  }
+}
